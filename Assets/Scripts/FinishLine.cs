5ce8cb70b25b612ca18c800926ba8fb028ef8fce
Rithvik Mandya <123717250+Gamedog0319@users.noreply.github.com>
2023-06-20 22:46:29 +0530

    Add files via upload

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class FinishLine : MonoBehaviour
+{
+    [SerializeField] float delay = 1f;
+    [SerializeField] ParticleSystem FinishEffect;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            FinishEffect.Play(); 
+            GetComponent<AudioSource>().Play();
+            Invoke("ReloadScene", delay);
+        }
+    }
+
+         void ReloadScene()
+        {
+            SceneManager.LoadScene(0);
+        }
+}
